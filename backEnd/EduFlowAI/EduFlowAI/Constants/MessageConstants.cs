fb5e4ae0ada5e8a9ffd8d80cde6e7973c5554c84
DHARSHAN MUTHUKUMAR <muthukumardharshan50@gmail.com>
2026-07-28 21:21:59 +0530

    constants addition to the backend

---
+++ backEnd/EduFlowAI/EduFlowAI/Constants/MessageConstants.cs
@@ -0,0 +1,19 @@
+﻿namespace EduFlowAI.Constants
+{
+    public static class MessageConstants
+    {
+        public const string LoginSuccessfull = "Login successful.";
+        
+        public const string InvalidCredentials = "Invalid credentials.";
+
+        public const string RegistrationSuccessfull = "Registration successful.";
+
+        public const string EmailAlreadyExists = "Email already exists.";
+
+        public const string UserNotFound = "User not found.";
+
+        public const string UnauthorizedAccess = "Unauthorized access.";
+
+
+    }
+}

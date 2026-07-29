709ddad2deecaf3d675dfd2c95afe97732dc94da
DHARSHAN MUTHUKUMAR <muthukumardharshan50@gmail.com>
2026-07-30 00:51:57 +0530

    Configuration file changes in backend

---
+++ backEnd/EduFlowAI/EduFlowAI/Configurations/JwtSettings.cs
@@ -0,0 +1,12 @@
+﻿namespace EduFlowAI.Configurations
+{
+    public sealed class JwtSettings
+    {
+        public const string SectionName = "Authentication:JWT";
+        public string Issuer { get; set; } = string.Empty;
+        public string Audience { get; set; } = string.Empty;
+        public string SecretKey { get; set; } = string.Empty;
+        public int AccessTokenExpiryMinutes { get; set; }
+        public int RefreshTokenExpiryDays { get; set; } 
+    }
+}

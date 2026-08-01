94f63bdf0c6df64cc37eb8de77349bfd25a0c9b2
DHARSHAN MUTHUKUMAR <muthukumardharshan50@gmail.com>
2026-08-02 02:20:50 +0530

    commit changes

---
+++ backEnd/EduFlowAI/EduFlowAI/Validators/RegisterRequestValidators.cs
@@ -0,0 +1,22 @@
+﻿using EduFlowAI.DTO.Authentiation.Requests;
+using FluentValidation;
+
+namespace EduFlowAI.Validators
+{
+    public class RegisterRequestValidators : AbstractValidator<RegisterRequestDTO>
+    {
+        public RegisterRequestValidators()
+        {
+
+            RuleFor(x => x.EmailId).NotEmpty().WithMessage("Email is required")
+                .EmailAddress().WithMessage("Invalid Email Address");
+
+            RuleFor(x => x.Password).NotEmpty().WithMessage("Password is required");
+
+            RuleFor(x => x.Username).NotEmpty().WithMessage("Username is required")
+                .MinimumLength(3).WithMessage("Username must be at least 3 characters long");
+
+
+        }
+    }
+}
